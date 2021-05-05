18d87695562425d61cb2af2d1723ac9221c9ac88
Sristi Chowdhury <53037053+Sristi27@users.noreply.github.com>
2021-05-06 00:38:50 +0530

    Jump Game  III - 1306

---
+++ Jump Game/Jump game iii.cpp
@@ -0,0 +1,69 @@
+1306. Jump Game III
+Given an array of non-negative integers arr, you are initially positioned at start index of the array. When you are at index i, you can jump to
+i + arr[i] or i - arr[i], check if you can reach to any index with value 0.
+
+Notice that you can not jump outside of the array at any time.
+
+ 
+
+Example 1:
+
+Input: arr = [4,2,3,0,3,1,2], start = 5
+Output: true
+Explanation: 
+All possible ways to reach at index 3 with value 0 are: 
+index 5 -> index 4 -> index 1 -> index 3 
+index 5 -> index 6 -> index 4 -> index 1 -> index 3 
+Example 2:
+
+Input: arr = [4,2,3,0,3,1,2], start = 0
+Output: true 
+Explanation: 
+One possible way to reach at index 3 with value 0 is: 
+index 0 -> index 4 -> index 1 -> index 3
+Example 3:
+
+Input: arr = [3,0,2,1,2], start = 2
+Output: false
+Explanation: There is no way to reach at index 1 with value 0.
+ 
+
+Constraints:
+
+1 <= arr.length <= 5 * 104
+0 <= arr[i] < arr.length
+0 <= start < arr.length
+
+
+
+
+SOLUTION
+
+
+
+class Solution {
+public:
+    bool canReach(vector<int>& arr, int start) {
+     //using bfs
+        queue<int>q;
+        int n=arr.size();
+        q.push(start);
+        while(!q.empty())
+        {
+            int temp=q.front();
+            q.pop();
+            if(temp<0 || temp>=n) return false;
+            if(arr[temp]==0) return true;
+            if(arr[temp]<0) continue;
+            
+            if(temp-arr[temp]>=0) q.push(temp-arr[temp]);
+            if(temp+arr[temp]<arr.size()) q.push(temp+arr[temp]);
+            
+            arr[temp]=-arr[temp];
+            
+        }
+        
+        
+        return false;
+    }
+};
