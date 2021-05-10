f504e104a0be530d8c1cd76259b5579e7429159c
Sristi Chowdhury <53037053+Sristi27@users.noreply.github.com>
2021-05-10 17:01:05 +0530

    Combination Sum IV - Leet 337

---
+++ Combination Sums/Combination Sum IV.cpp
@@ -0,0 +1,65 @@
+377. Combination Sum IV
+Medium
+Given an array of distinct integers nums and a target integer target, return the number of possible combinations that add up to target.
+
+The answer is guaranteed to fit in a 32-bit integer.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3], target = 4
+Output: 7
+Explanation:
+The possible combination ways are:
+(1, 1, 1, 1)
+(1, 1, 2)
+(1, 2, 1)
+(1, 3)
+(2, 1, 1)
+(2, 2)
+(3, 1)
+Note that different sequences are counted as different combinations.
+Example 2:
+
+Input: nums = [9], target = 3
+Output: 0
+ 
+
+Constraints:
+
+1 <= nums.length <= 200
+1 <= nums[i] <= 1000
+All the elements of nums are unique.
+1 <= target <= 1000
+ 
+
+Follow up: What if negative numbers are allowed in the given array? How does it change the problem? What limitation we need to add to the question to allow negative numbers?
+
+  
+  
+  Solution:
+
+
+class Solution {
+public:
+    int findComb(vector<int>&nums,int target,vector<int>&dp)
+    {
+        if(target==0)
+            return 1;
+        if(dp[target]!=-1)
+            return dp[target];
+        int ans=0;
+        for(int i=0;i<nums.size();i++)
+        {
+        if(nums[i]<=target)
+            ans+=findComb(nums,target-nums[i],dp);
+        }
+            dp[target]=ans;
+        return ans;
+        }
+    int combinationSum4(vector<int>& nums, int target) {
+        vector<int> dp(target+1,-1);
+        return findComb(nums,target,dp);
+    }
+};
